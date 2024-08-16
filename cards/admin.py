4e10fec72db32b9d9ca8ac5996d5467b367aacc3
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-16 20:46:39 +0400

    lesson_50: добавил примеры для настройки отображения админки

--- cards/admin.py
+++ cards/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Card
 
 
+@admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    pass
-
-
-admin.site.register(Card, CardAdmin)
+    list_display = ('question', 'answer', 'upload_date', 'views', 'adds',)
+    list_filter = ('upload_date', 'views', 'adds',)
+    list_per_page = 10
+    search_fields = ('answer',)
+    fields = (('question', 'answer'), ('views', 'adds'))
