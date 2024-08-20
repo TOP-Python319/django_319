3db54f7ec038e11f3e177c33fd7717b21d05f9b3
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-20 21:53:52 +0400

    lesson_51: добавили конвертацию MD в HTML и подсветку кода

--- cards/templatetags/markdown_to_html.py
+++ cards/templatetags/markdown_to_html.py
@@ -1,4 +1,6 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 
 # в Django это регистрация библиотеки шаблонов, которая позволяет использовать пользовательские теги
@@ -8,6 +10,18 @@ register = template.Library()
 
 # @register это декоратор, который используется в Django для создания простых пользовательских тегов шаблонов.
 # simple_tag - это декоратор, который используется для создания простых пользовательских тегов шаблонов.
-@register.simple_tag
+@register.simple_tag(name='markdown_to_html')
 def markdown_to_html(markdown_text: str) -> str:
-    return markdown_text.upper()  # просто для теста
+    '''
+    Преобразование Markdown в HTML
+    :param markdown_text: текст Markdown
+    :return: HTML
+    '''
+
+    # включение расширений Markdown
+    md_extensions = ['extra', 'fenced_code', 'tables']
+
+    # преобразование Markdown в HTML
+    html = markdown.markdown(markdown_text, extensions=md_extensions)
+
+    return mark_safe(html)
