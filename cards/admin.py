b6f04e23fec245605a1281b34c4bb4f1494cc07b
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-27 20:16:12 +0400

    lesson_53: дополнительные настройки амдин. панели

--- cards/admin.py
+++ cards/admin.py
@@ -5,13 +5,22 @@ from .models import Card
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     # поля, которые будут отображаться в админке
-    list_display = ('question', 'category', 'views', 'upload_date')
+    list_display = ('pk', 'question', 'category', 'views', 'upload_date',)
 
     # поля, которые будут ссылками
-    list_display_links = ('question', 'category',)
+    list_display_links = ('pk', 'category',)
 
     # поля, которые будут включены в поиск
     search_fields = ('answer',)  # не забываем поставить запятую в конце, если у нас только одно значение, что показать Python, что это кортеж
 
-    # поля, которые будут фильтроваться
+    # поля, по которым можно фильтровать
     list_filter = ('category',)
+
+    # сортировка по умолчанию по какому полю и в каком направлении
+    ordering = ('category', '-views', )
+
+    # постраничная навигация (пагинация, пейджинация)
+    list_per_page = 18
+
+    # редактируемые поля (без проваливания в карточку)
+    list_editable = ('question', 'views', )
