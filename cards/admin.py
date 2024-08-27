a0c69eac62ec5272bc0ac65f43bdafd833b381f7
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-27 21:41:32 +0400

    lesson_53: добавили в админ. панель поле наличие кода и дополнительные действия

--- cards/admin.py
+++ cards/admin.py
@@ -5,7 +5,7 @@ from .models import Card
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     # поля, которые будут отображаться в админке
-    list_display = ('pk', 'question', 'category', 'views', 'upload_date', 'status',)
+    list_display = ('pk', 'question', 'category', 'views', 'upload_date', 'status', 'has_code')
 
     # поля, которые будут ссылками
     list_display_links = ('pk', 'category',)
@@ -20,7 +20,24 @@ class CardAdmin(admin.ModelAdmin):
     ordering = ('category', '-views',)
 
     # постраничная навигация (пагинация, пейджинация)
-    list_per_page = 18
+    list_per_page = 10
 
     # редактируемые поля (без проваливания в карточку)
     list_editable = ('question', 'views', 'status',)
+
+    # действия с карточками
+    actions = ('set_checked', 'set_unchecked')
+
+    @admin.display(description='Наличие кода')
+    def has_code(self, card):
+        return 'Да' if '```' in card.answer else 'Нет'
+
+    @admin.action(description='Отметить карточку как проверенную')
+    def set_checked(self, request, queryset):
+        updated = queryset.update(status=Card.Status.CHECKED)
+        self.message_user(request, f'{updated} карточек было отмечено как проверенные')
+
+    @admin.action(description='Отметить карточку как не проверенную')
+    def set_unchecked(self, request, queryset):
+        updated = queryset.update(status=Card.Status.UNCHECKED)
+        self.message_user(request, f'{updated} карточек было отмечено как не проверенные')
