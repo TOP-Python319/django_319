ba4ce4f6c5ad7cf2013fb608a96475e6f8c28d3c
Maks Iamchuk <maks.iamchuk@outlook.com>
2024-08-27 20:02:28 +0400

    lesson_53: провели первоночальную настройку админ. панели

--- cards/admin.py
+++ cards/admin.py
@@ -4,9 +4,14 @@ from .models import Card
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    # list_display = ('question', 'answer', 'upload_date', 'views', 'adds',)
-    # list_filter = ('upload_date', 'views', 'adds',)
-    # list_per_page = 10
-    # search_fields = ('answer',)
-    # fields = (('question', 'answer'), ('views', 'adds'))
-    pass
+    # поля, которые будут отображаться в админке
+    list_display = ('question', 'category', 'views', 'upload_date')
+
+    # поля, которые будут ссылками
+    list_display_links = ('question', 'category',)
+
+    # поля, которые будут включены в поиск
+    search_fields = ('answer',)  # не забываем поставить запятую в конце, если у нас только одно значение, что показать Python, что это кортеж
+
+    # поля, которые будут фильтроваться
+    list_filter = ('category',)
